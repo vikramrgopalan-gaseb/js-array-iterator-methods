f40e2debf9ece1144c5fa78fb63faacafd7cbb3b
vikramgopalan-gaseb <vikram.r.gopalan@gmail.com>
2025-10-07 11:33:29 -0600

    10/5 ex 8

--- app.js
+++ app.js
@@ -334,3 +334,24 @@ const isEveryone19OrOlder = devs.every((dev) => {
 console.log('Exercise 7 my result: ', isEveryone19OrOlder);
 console.log('Exercise 7 correct result: ', false);
 
+/*
+Exercise 8: Array.prototype.find()
+
+Use Array.prototype.find() to identify and retrieve the comment object with 
+a specific ID 823423 from an array of comment objects.
+
+- Assign the found comment object to the variable 'commentById'.
+*/
+
+// let commentById = {};
+
+// Complete the exercise in the space below:
+
+const commentById = comments.find((comment) => {
+    return comment.id === 823423;
+})
+
+// Check your work:
+console.log('Exercise 8 my result: ', commentById);
+console.log('Exercise 8 correct result: ', { text: 'Super good', id: 823423 });
+
